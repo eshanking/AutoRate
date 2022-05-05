6fbdebdf779120836e99ad29bcece6130d985dc3
King <kinge2@lri-108705.cc.ad.cchs.net>
2022-05-05 11:19:35 -0400

    added ability to load plate data

--- AutoRate.py
+++ AutoRate.py
@@ -1,9 +1,35 @@
 import pandas as pd
+import os
+
+class Experiment():
+    def __init__(self,folder_path,moat=False):
+        self.moat = moat
+        self.folder_path = folder_path
+        self.plate_data_paths = self.load_plate_data()
+        self.plates = []
+
+        for pdp in self.plate_data_paths:
+            self.plates.append(Plate(pdp))
+
+    def load_plate_data(self):
+        plate_files = sim_files = os.listdir(path=self.folder_path)
+        plate_files.sort()
+
+        plate_data_paths = []
+
+        for pf in plate_files:
+            if pf != '.DS_Store':
+                plate_path = self.folder_path + os.sep + pf
+                plate_data_paths.append(plate_path)
+
+        return plate_data_paths
+
 
 class Plate():
     def __init__(self,data_path,moat=False):
 
         self.moat = moat
+        # print(data_path)
         self.data = pd.read_csv(data_path)
 
         self.background_keys = self.get_background_keys()
@@ -41,8 +67,11 @@ class Plate():
             list: list of keys
         """
         bg_keys = self.get_background_keys()
-
-        data_keys = [k for k in self.data.keys() if k not in bg_keys]
+        if self.background_keys is None:
+            data_keys = self.data.keys()
+        else:
+            data_keys = [k for k in self.data.keys() if k not in self.background_keys]
+        
         data_keys = data_keys[2:]
 
         return data_keys
