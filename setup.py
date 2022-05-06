fd6d1cf8346b7e76af6b2e2207573b6f390c0dff
Weaver <WEAVERD3@ccf.org>
2022-05-06 13:42:47 -0400

    fix dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(
     packages=['autorate', "autorate.test", "autorate.test.data"], 
     install_requires = [
       "pandas",
-      "pytest"
+      "pytest",
+      "scipy",
+      "matplotlib",
+      "numpy"
     ],
     include_package_data=True,
     package_data={'': ['data/*.csv']}
